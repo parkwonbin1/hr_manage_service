2d9ccac25653546f8172f2e528794a17b5814c68
parkwonbin1 <qkrdnjs1219@gmail.com>
2025-10-20 15:13:20 +0900

    2nd commit

---
+++ hr_project/__init__.py
@@ -0,0 +1,5 @@
+# hr_manage_service/__init__.py
+
+import pymysql
+
+pymysql.install_as_MySQLdb()
--- hr_project/settings.py
+++ hr_project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'hr_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',  # 이렇게 변경!
+        'NAME': 'hr_manage_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
